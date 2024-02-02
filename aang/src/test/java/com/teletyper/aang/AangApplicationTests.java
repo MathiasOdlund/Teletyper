1e2975c816b5bdf2933c191b99564d83e6b1b7b7
Mathias Ödlund <mathias.odlund@telenor.no>
2024-02-02 19:39:15 +0100

    Init - To be continued

---
+++ aang/src/test/java/com/teletyper/aang/AangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teletyper.aang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
