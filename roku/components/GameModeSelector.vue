96ec99b70cb5ff557e172e20740a26740d51187e
Mathias Ödlund <mathias.odlund@telenor.no>
2024-02-04 19:34:35 +0100

    Layout changes

--- roku/components/GameModeSelector.vue
+++ roku/components/GameModeSelector.vue
@@ -2,7 +2,24 @@
   <div
     class="flex flex-row gap-2 items-center bg-base-100 justify-center px-10 py-4 shadow-sm margin-auto rounded-md mb-4"
   >
-    <p class="btn btn-ghost">Wordlist</p>
+    <button class="btn">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25"
+        />
+      </svg>
+
+      Wordlist
+    </button>
   </div>
 </template>
 
